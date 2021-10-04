26db11462ece7f51c8f00cc3259c561cf5c3ef5e
Romina <83961643+rominalodolo@users.noreply.github.com>
2021-10-04 21:54:35 +0200

    Add files via upload

---
+++ TryItYourself/Chap02.py
@@ -0,0 +1,61 @@
+#2.1
+greetings = 'Hello World'
+print(greetings)
+
+
+#2.2
+message = "Hi, You're cute!"
+print(message)
+message = "What's up!"
+print(message)
+
+
+
+#2.3
+name = "romina"
+personal = "Hello {}, would you like to learn some Python today?.".format(name.title())
+print(personal)
+
+
+#2.4
+fullName = 'romina lodolo'
+print(fullName.title())
+print(fullName.upper())
+print(fullName.lower()) 
+
+
+#2.5 
+print('Romina Lodolo once said, "Water should be free because it grows on trees".')
+
+
+#2.6
+quote = '"Water should be free because it grows on trees"'
+famous_person = "Romina Lodolo"
+famousQuote = f"{famous_person} once said, {quote}"
+print(famousQuote)
+
+
+#2.7 
+friend = " sally "
+friend = f"{friend} \n\t{friend.lstrip()} \n\t{friend.rstrip()} \n\t{friend.strip()}" 
+print(friend)
+
+
+#2.8 
+print(2 + 6)
+print(9 - 1)
+print(2 * 4)
+print(8 / 1)
+
+
+#2.9 
+favNum = 6
+print(f"My favourite number is {favNum}")
+
+
+#2.10 Adding Comments: 
+#using this hashtag infront of a line makes it a comment :) 
+
+
+#2.11 Zen of Python: 
+import this
