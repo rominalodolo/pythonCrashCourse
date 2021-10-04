16cd4e5a67627d37490ed3cb7e089ddc10fdf382
Romina <83961643+rominalodolo@users.noreply.github.com>
2021-10-04 19:37:47 +0200

    Add files via upload

---
+++ TryItYourself/Chap01.py
@@ -0,0 +1,16 @@
+#1.1 python.org:
+# Explore the Python home page (https://python.org/) to find topics that interest
+#you. As you become familiar with Python, different parts of the site will be more useful to you.
+
+
+#1.2 Hello World Typos
+print("Hello world!')
+#statement generates syntax error due to closing quotation marks not being the same 
+
+print"Hello world!")
+#statement generates syntax error due to missing opening bracket 
+
+
+#1.3 Infinite Skills:
+#There are way too many ideas in my head to even list here but I already keep a journal of it all. I will create and build programs that inspire others in the future to come. 
+
