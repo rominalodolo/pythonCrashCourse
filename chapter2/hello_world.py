ff4440594ccaa45d834392b158f627495f7aea5a
Romina <83961643+rominalodolo@users.noreply.github.com>
2021-10-04 19:26:31 +0200

    Add files via upload

---
+++ chapter2/hello_world.py
@@ -0,0 +1,8 @@
+message = "Hello world!"
+number = 25
+first_name = "John"
+print(message)
+print(number)
+print(first_name)
+
+
---
+++ chapter2/name.py
@@ -0,0 +1,46 @@
+name = 'John Wick'
+print(name.title())
+print(name.upper())
+print(name.lower()) 
+
+first_name = "romina"
+last_name = "lodolo"
+
+print(f"Full Name: {first_name} {last_name}")
+print(f"Full Name: {first_name.title()} {last_name.title()}")
+
+first_name = "jane"
+last_name = "doe"
+full_name = f"{first_name} {last_name}"
+print(full_name.title())
+
+full_name = "My name is {} and my surname is {}.".format(first_name.title(), last_name.title())
+print(full_name)
+
+print("\n\n")
+language1 = "Python"
+language2 = "C++"
+language3 = "JavaScript"
+print("Languages:\nPython\nC\nJavaScript")
+print("Languages:\n\tPython\n\tSwift\n\tJavaScript")
+print(f"Languages: \n\t{language1}\n\t{language2}\n\t{language3}")
+
+
+print("\n\n")
+favorite_language = ' python '
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
+
+
+universe_age = 14_000_000_000
+print(universe_age)
+
+x, y, z = 2, 4, 6
+print(f"X = {x} \nY = {y} \nZ = {z}")
+
+
+MAX_CONNECTIONS = 5000
+
+MONTH = "October" 
+
