a17de4cdcbfd53d47425d4b43f06c9bbdfebdb11
rominalodolo <lodoloromina@gmail.com>
2022-01-24 22:14:12 +0200

    update

--- TryItYourself/Chap04.py
+++ TryItYourself/Chap04.py
@@ -67,7 +67,11 @@ written as 2**3 in Python. Make a list of the first 10 cubes (that is, the cube 
 from 1 through 10), and use a for loop to print out the value of each cube.
 
 
-#4.9 Cube Comprehension: Use a list comprehension to generate a list of the first 10 cubes.
+#4.9 Cube Comprehension: 
+cubes = [number**3 for number in range(1,11)]
+
+for cube in cubes:
+    print(cube)
 
 #4.10 Slices: Using one of the programs you wrote in this chapter, add several lines to the end
 of the program that do the following:
@@ -79,15 +83,20 @@ Print the message The last three items in the list are:. Use a slice to
 print the last three items in the list.
 
 #4.11
- My Pizzas, Your Pizzas: Start with your program from Exercise 4-1 (page 56). Make a
-copy of the list of pizzas, and call it friend_pizzas. Then, do the following:
-Add a new pizza to the original list.
-Add a different pizza to the list friend_pizzas.
-Prove that you have two separate lists. Print the message My
-favorite pizzas are:, and then use a for loop to print the first list.
-Print the message My friend’s favorite pizzas are:, and then use a for
-loop to print the second list. Make sure each new pizza is stored in
-the appropriate list.
+myPizzas = ['Margarita', 'Vegetarian', '4Cheeses']
+friend_pizzas = myPizzas[:]
+
+myPizzas.append("vegan")
+friend_pizzas.append('avo & feta')
+
+print("My favorite pizzas are:")
+for pizza in myPizzas:
+    print(f"- {pizza}")
+
+print("\nMy friend's favorite pizzas are:")
+for pizza in friend_pizzas:
+    print(f"- {pizza}")
+
 
 #4.12 More Loops: All versions of foods.py in this section have avoided using for loops when
 printing to save space. Choose a version of foods.py, and write two for loops to print each list of
@@ -96,20 +105,20 @@ foods.
 #4.13 Buffet:
 
 basic_menu = (
-    "beyond beef burger", "veggie spring roll", "grilled cheese", "ice-cream sunday", "",
+    "beyond beef burger", "veggie spring roll", "grilled cheese", "ice-cream sunday", "ice-coffee",
 )
 
 print("These are the items you can choose from: ")
 for item in basic_menu:
     print(f"- {item}")
 
+print("\nOur menu has been updated.")
 
-Use a for loop to print each food the restaurant offers.
-Try to modify one of the items, and make sure that Python rejects
-the change.
-The restaurant changes its menu, replacing two of the items with
-different foods. Add a line that rewrites the tuple, and then use a
-for loop to print each of the items on the revised menu.
+# Try to modify one of the items, and make sure that Python rejects
+# the change.
+# The restaurant changes its menu, replacing two of the items with
+# different foods. Add a line that rewrites the tuple, and then use a
+# for loop to print each of the items on the revised menu.
 
 #4.14
 # PEP 8: Look through the original PEP 8 style guide at https://python.org/dev/peps/pep-0008/. 
