5430d866a932e7e758529308d2f58e6e724de85e
rominalodolo <lodoloromina@gmail.com>
2022-01-24 23:18:35 +0200

    update

--- TryItYourself/Chap08.py
+++ TryItYourself/Chap08.py
@@ -1,57 +1,93 @@
 #8.1 Message: 
 
-Write a function called display_message() that prints one sentence telling
-everyone what you are learning about in this chapter. Call the function, and make sure the
-message displays correctly.
+from pydoc import text
+
+
+def display_message():
+    msg = "I'm learning about functions."
+    print(msg)
+
+display_message()
 
 
 #8.2 Favorite Book: 
 
-Write a function called favorite_book() that accepts one parameter,
-title. The function should print a message, such as One of my favorite books is Alice in
-Wonderland. Call the function, making sure to include a book title as an argument in the
-function call.
+def favorite_book(title):
+    print(f"One of my favorite books is {title}.")
+
+favorite_book("Alice in Wonderland")
+
 
 
 #8.3 T-Shirt: 
-Write a function called make_shirt() that accepts a size and the text of a
-message that should be printed on the shirt. The function should print a sentence summarizing
-the size of the shirt and the message printed on it.
-Call the function once using positional arguments to make a shirt. Call the function a second
-time using keyword arguments.
+
+def make_shirt(size, text):
+    print(f"\nThe size of the shirt is {size}.")
+    print(f"Message on the shirt, '{text}'.")
+
+make_shirt('medium', 'Coding is fun!')
+make_shirt(text="Blue Light Glasses are COOL", size='small')
 
 
 #8.4 Large Shirts: 
-Modify the make_shirt() function so that shirts are large by default with a
-message that reads I love Python. Make a large shirt and a medium shirt with the default
-message, and a shirt of any size with a different message.
+
+def make_shirt(size='large', text='I love Python'):
+    print(f"\nThe size of the T-shirt is {size}.")
+    print(f'Message on the shirt, "{text}"')
+
+make_shirt()
+make_shirt(size='medium')
+make_shirt('small', 'Coding is fun!')
 
 
 #8.5 Cities: 
-Write a function called describe_city() that accepts the name of a city and its
-country. The function should print a simple sentence, such as Reykjavik is in Iceland. Give
-the parameter for the country a default value. Call your function for three different cities, at
-least one of which is not in the default country.
+
+def describe_city(city, country='malta'):
+    msg = f"{city.title()} is in {country.title()}."
+    print(msg)
+
+describe_city('selima')
+describe_city('reykjavik', 'iceland')
+describe_city('birgu')
 
 
 #8.6 City Names: 
-Write a function called city_country() that takes in the name of a city and
-its country. The function should return a string formatted like this:
-"Santiago, Chile"
-Call your function with at least three city-country pairs, and print the values that are
-returned.
+
+def city_country(city, country):
+    return f"{city.title()}, {country.title()}"
+
+city = city_country('santiago', 'chile')
+print(city)
+
+city = city_country('pretoria', 'south africa')
+print(city)
+
+city = city_country('kolkata', 'india')
+print(city)
 
 
 #8.7 Album: 
-Write a function called make_album() that builds a dictionary describing a music
-album. The function should take in an artist name and an album title, and it should return a
-dictionary containing these two pieces of information. Use the function to make three
-dictionaries representing different albums. Print each return value to show that the dictionaries
-are storing the album information correctly.
+
 Use None to add an optional parameter to make_album() that allows you to store the number
 of songs on an album. If the calling line includes a value for the number of songs, add that value
 to the album’s dictionary. Make at least one new function call that includes the number of
 songs on an album.
+
+def make_album(artist, title):
+    album_dict = {
+        'artist': artist.title(),
+        'title': title.title(),
+        }
+    return album_dict
+
+album = make_album('nothing,nowhere.', 'trauma factory')
+print(album)
+
+album = make_album('jutes', 'careful what you wish for')
+print(album)
+
+album = make_album('vanessa hudgens', 'v')
+print(album)
 
 
 #8.8 User Albums
@@ -78,7 +114,8 @@ send_messages() with a copy of the list of messages. After calling the function,
 your lists to show that the original list has retained its messages.
 
 
-8-12. Sandwiches: Write a function that accepts a list of items a person wants on a sandwich.
+#8.12 Sandwiches: 
+Write a function that accepts a list of items a person wants on a sandwich.
 The function should have one parameter that collects as many items as the function call
 provides, and it should print a summary of the sandwich that’s being ordered. Call the function
 three times, using a different number of arguments each time.
@@ -89,12 +126,15 @@ yourself by calling build_profile(), using your first and last names and three o
 pairs that describe you.
 
 
-8-14. Cars: Write a function that stores information about a car in a dictionary. The function
-should always receive a manufacturer and a model name. It should then accept an arbitrary
+# 8.14 Cars: 
+Write a function that stores information about a car in a dictionary. 
+The function should always receive a manufacturer and a model name. It should then accept an arbitrary
 number of keyword arguments. Call the function with the required information and two other
 name-value pairs, such as a color or an optional feature. Your function should work for a call
 like this one:
+
 car = make_car('subaru', 'outback', color='blue', tow_package=True)
+
 Print the dictionary that’s returned to make sure all the information was stored correctly.
 
 
@@ -102,15 +142,18 @@ Print the dictionary that’s returned to make sure all the information was stor
 called printing_functions.py. Write an import statement at the top of printing_models.py, and
 modify the file to use the imported functions.
 
-8-16. Imports: Using a program you wrote that has one function in it, store that function in a
+#8.16 Imports: 
+Using a program you wrote that has one function in it, store that function in a
 separate file. Import the function into your main program file, and call the function using each
 of these approaches:
+
 import module_name
 from module_name import function_name
 from module_name import function_name as fn
 import module_name as mn
 from module_name import *
 
-# 8-17. Styling Functions: Choose any three programs you wrote for this chapter
+# 8.17 Styling Functions: 
+# Choose any three programs you wrote for this chapter
 # make sure they follow the styling guidelines described in this section
 
