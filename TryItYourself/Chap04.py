1b9289fef36bbf620613360b25d664b3c3359da5
rominalodolo <lodoloromina@gmail.com>
2022-01-24 21:56:20 +0200

    update

--- TryItYourself/Chap04.py
+++ TryItYourself/Chap04.py
@@ -37,13 +37,18 @@ print(f"Any of these animals would make a great pet!")
 print("\n")
 
 
-#4.3
- Counting to Twenty: Use a for loop to print the numbers from 1 to 20, inclusive.
+#4.3 Counting to Twenty
+numbers = list(range(1, 21))
 
+for number in numbers:
+    print(number)
 
-#4.4 One Million: Make a list of the numbers from one to one million, and then use a for loop
-to print the numbers. (If the output is taking too long, stop it by pressing CTRL-C or by
-closing the output window.)
+#4.4 One Million: 
+numbers = list(range(1, 1000001))
+
+for number in numbers:
+    print(number)
+# (If the output is taking too long, stop it by pressing CTRL-C or by closing the output window.)
 
 #4.5 Summing a Million: Make a list of the numbers from one to one million, and then use
 min() and max() to make sure your list actually starts at one and ends at one million. Also, use
