e5b554877e3a9066cc327c9bd235ee81f26aefe8
rominalodolo <lodoloromina@gmail.com>
2022-01-25 13:11:24 +0200

    update

--- TryItYourself/Chap05.py
+++ TryItYourself/Chap05.py
@@ -1,5 +1,5 @@
-#5.1 
-Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+#5.1 Conditional Tests
+Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
 car = 'subaru'
 print("Is car == 'subaru'? I predict True.")
 print(car == 'subaru')
@@ -23,26 +23,51 @@ Test whether an item is in a list
 Test whether an item is not in a list
 
 
-#5.3 
-Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called
-alien_color and assign it a value of 'green', 'yellow', or 'red'.
-Write an if statement to test whether the alien’s color is green. If
-it is, print a message that the player just earned 5 points.
-Write one version of this program that passes the if test and
-another that fails. (The version that fails will have no output.)
+#5.3 Alien Colors 
+# Imagine an alien was just shot down in a game.
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+
+
+# no output version 
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+
+
 
 #5.4 
-Alien Colors #2: Choose a color for an alien as you did in Exercise 5-3, and write an ifelse
-chain.
-If the alien’s color is green, print a statement that the player just
-earned 5 points for shooting the alien.
-If the alien’s color isn’t green, print a statement that the player just
-earned 10 points.
-Write one version of this program that runs the if block and
-another that runs the else block.
+# if 
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
+
+# else 
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
+
 
 
 #5.5
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+elif alien_color == 'yellow':
+    print("You just earned 10 points!")
+else:
+    print("You just earned 15 points!")
 Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-elif-else chain.
 If the alien is green, print a message that the player earned 5
 points.
@@ -53,60 +78,88 @@ Write three versions of this program, making sure each message is
 printed for the appropriate color alien.
 
 #5.6
-Stages of Life: Write an if-elif-else chain that determines a person’s stage of life. Set a
-value for the variable age, and then:
-If the person is less than 2 years old, print a message that the
-person is a baby.
-If the person is at least 2 years old but less than 4, print a message
-that the person is a toddler.
-If the person is at least 4 years old but less than 13, print a message
-that the person is a kid.
-If the person is at least 13 years old but less than 20, print a
-message that the person is a teenager.
-If the person is at least 20 years old but less than 65, print a
-message that the person is an adult.
-If the person is age 65 or older, print a message that the person is
-an elder.
 
-#5.7 
-Favorite Fruit: Make a list of your favorite fruits, and then write a series of independent
-if statements that check for certain fruits in your list.
-Make a list of your three favorite fruits and call it favorite_fruits.
-Write five if statements. Each should check whether a certain kind
-of fruit is in your list. If the fruit is in your list, the if block should
-print a statement, such as You really like bananas!
+age = 27
 
-#5.8
-Hello Admin: Make a list of five or more usernames, including the name 'admin'.
-Imagine you are writing code that will print a greeting to each user after they log in to a
-website. Loop through the list, and print a greeting to each user:
-If the username is 'admin', print a special greeting, such as Hello
-admin, would you like to see a status report?
-Otherwise, print a generic greeting, such as Hello Jaden, thank you
-for logging in again.
+if age < 2:
+    print("You're a baby!")
+elif age < 4:
+    print("You're a toddler!")
+elif age < 13:
+    print("You're a kid!")
+elif age < 20:
+    print("You're a teenager!")
+elif age < 65:
+    print("You're an adult!")
+else:
+    print("You're an elder!")
 
-#5.9
-No Users: Add an if test to hello_admin.py to make sure the list of users is not empty.
-If the list is empty, print the message We need to find some users!
-Remove all of the usernames from your list, and make sure the
-correct message is printed.
 
-#5-10
- Checking Usernames: Do the following to create a program that simulates how
-websites ensure that everyone has a unique username.
-Make a list of five or more usernames called current_users.
-Make another list of five usernames called new_users. Make sure one
-or two of the new usernames are also in the current_users list.
-Loop through the new_users list to see if each new username has
-already been used. If it has, print a message that the person will
-need to enter a new username. If a username has not been used,
-print a message saying that the username is available.
-Make sure your comparison is case insensitive. If 'John' has been
-used, 'JOHN' should not be accepted. (To do this, you’ll need to
-make a copy of current_users containing the lowercase versions of all
-existing users.)
+#5.7 Favorite Fruit
+favorite_fruits = ['blueberries', 'mangos', 'strawberries']
 
-#5-11 Ordinal Numbers: 
+if 'bananas' in favorite_fruits:
+    print("You really like bananas!")
+if 'apples' in favorite_fruits:
+    print("You really like apples!")
+if 'blueberries' in favorite_fruits:
+    print("You really like blueberries!")
+if 'kiwis' in favorite_fruits:
+    print("You really like kiwis!")
+if 'strawberries' in favorite_fruits:
+    print("You really like strawberries!")
+
+
+#5.8 Hello Admin: 
+
+usernames = ['romi', 'sally', 'admin', 'tay', 'john']
+
+for username in usernames:
+    if username == 'admin':
+        print("Hello admin, would you like to see a status report?")
+    else:
+        print(f"Hello {username}, thank you for logging in again.")
+
+#5.9 No Users
+usernames = []
+
+if usernames:
+    for username in usernames:
+        if username == 'admin':
+            print("Hello admin, would you like to see a status report?")
+        else:
+            print(f"Hello {username}, thank you for loggin in again!")
+else:
+    print("We need to find some users!")
+
+
+#5.10 Checking Usernames
+
+current_users = ['romi', 'sally', 'admin', 'tay', 'John']
+new_users = ['Bianca', 'Johah', 'Sally', 'kate', 'Irana']
+
+current_users_lower = [user.lower() for user in current_users]
+
+for new_user in new_users:
+    if new_user.lower() in current_users_lower:
+        print(f"Sorry {new_user}, that name is taken.")
+    else:
+        print(f"Great, {new_user} is still available.")
+
+
+#5.11 Ordinal Numbers
+numbers = list(range(1,10))
+
+for number in numbers:
+    if number == 1:
+        print("1st")
+    elif number == 2:
+        print("2nd")
+    elif number == 3:
+        print("3rd")
+    else:
+        print(f"{number}th")
+
 Ordinal numbers indicate their position in a list, such as 1st or 2nd.
 Most ordinal numbers end in th, except 1, 2, and 3.
 Store the numbers 1 through 9 in a list.
